883544b66f581f070dc0ea40c816b7ac4363bbbb
George Badulescu <georgebadulescu123@gmail.com>
2024-01-03 12:33:49 -0700

    Update landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,12 @@
-// Import necessary dependencies and components from React and other modules
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import IndexPage from './pages/IndexPage';
-import GamePage from './pages/GamePage'
-
-
-
-
-
-import axios from 'axios';
-import { UserContextProvider } from './UserContext';
-
-// Configure the default base URL and credentials for axios requests
-axios.defaults.baseURL = 'http://localhost:4000';
-axios.defaults.withCredentials = true;
-
-// Define the main App component
+import Hero from "./components/Hero"
 function App() {
+
+
   return (
-    <UserContextProvider>
-      <Routes>
-        {/* Creates routes for different locations in the app */}
-        <Route index element={<IndexPage />} /> // Define the index page route
-        <Route path="/game" element={<GamePage/>} />
-      </Routes>
-    </UserContextProvider>
-  );
+   <div>
+   <Hero/>
+   </div>
+  )
 }
 
-export default App;
+export default App
