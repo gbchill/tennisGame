ba64761dc43ff42e63e223a7d9c7d70592e3a4f3
George Badulescu <georgebadulescu123@gmail.com>
2024-01-03 13:46:09 -0700

    Update Landing Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import Hero from "./components/Hero"
+import styled from "styled-components";
+import Hero from "./components/Hero";
+
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
 function App() {
-
-
   return (
-   <div>
-   <Hero/>
-   </div>
-  )
+    <Container>
+      <Hero />
+    </Container>
+  );
 }
 
-export default App
+export default App;
